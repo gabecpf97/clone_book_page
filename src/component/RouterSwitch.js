ce007dcf21f0bd051aa883ad31c2ee84763bbc40
gabecpf97 <gabrielcpf2997@gmail.com>
2021-12-28 00:07:32 -0800

    added routerswitch

---
+++ src/component/RouterSwitch.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
+const RouterSwitch = () => {
+
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/clone_book_page/" element={} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouterSwitch;
