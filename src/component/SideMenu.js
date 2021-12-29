637d80d4bdc288c743b0a156f2815493ccf84f9f
gabecpf97 <gabrielcpf2997@gmail.com>
2021-12-29 00:42:34 -0800

    added basic for home page

---
+++ src/component/SideMenu.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SideMenu = () => {
+
+    return (
+        <div className="side_menu">
+            <ul>
+                <li>Account</li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default SideMenu;
