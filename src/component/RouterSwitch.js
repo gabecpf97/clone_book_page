0ea4a69b80593293d801dd82185639b52d5b696a
gabecpf97 <gabrielcpf2997@gmail.com>
2021-12-29 00:37:16 -0800

    added to router

--- src/component/RouterSwitch.js
+++ src/component/RouterSwitch.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import LogIn from './LogIn';
+import SignUp from './SignUp';
 
 const RouterSwitch = () => {
 
@@ -7,6 +9,8 @@ const RouterSwitch = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/clone_book_page/" element={} />
+                <Route path="/clone_book_page/sign_up" element={<SignUp />} />
+                <Route path="/clone_book_page/log_in" element={<LogIn />} />
             </Routes>
         </BrowserRouter>
     )
